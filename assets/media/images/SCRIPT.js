df7074eeef1982fe0465d476e052e0313ff8ddbf
anowa-eng <60952646+anowa-eng@users.noreply.github.com>
2020-08-19 16:56:21 -0700

    Delete SCRIPT.js

--- assets/media/images/SCRIPT.js
+++
@@ -1,5 +0,0 @@
-src = function(name, target) {
-  if (!(name || target)) throw new Error('Parameter(s) are undefined');
-  window[`image_${name}`] = new Image(100,100);
-  window[`image_${name}`].src = target;
-}
