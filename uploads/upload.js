c35fca3509ffcee7d5abe1cd00a1dad7afddfba1
anowa-eng <60952646+anowa-eng@users.noreply.github.com>
2020-09-03 16:42:37 -0700

    Update upload.js

--- uploads/upload.js
+++ uploads/upload.js
@@ -2,4 +2,7 @@ let formData = new FormData();
 upload = [];
 upload.push(document.querySelector('body > div > div.editor > table > tbody > tr:nth-child(3) > td > button > form > input:nth-child(1)'));
 formData.append("file", upload[0].files[0]);
-await fetch('/uploads', {method: "GET", body: formData});
+fetch('/uploads', {method: "GET", body: formData})
+  .then((response) => {
+    alert(JSON.parse(response))
+  });
