f95d4dd94b52cc3187bf8106f3254ed5a915619e
anowa-eng <60952646+anowa-eng@users.noreply.github.com>
2020-09-03 17:21:29 -0700

    Update upload.js

--- uploads/upload.js
+++ uploads/upload.js
@@ -1,5 +1,5 @@
 // Trying to recreate PHP $_POST using JS
-var $_POST = () => {
+function $_POST() {
   if (arguments.length == 0) return document.querySelectorAll('*[name]');
   else if (arguments.length > 1) throw new Error('$_POST() needs only one parameter')
   else return document.querySelectorAll(`*[name="${arguments[0]}"]`)
