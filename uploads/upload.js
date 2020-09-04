fff18c809f79487b5dba59ddf50cdebf89c52fe0
anowa-eng <60952646+anowa-eng@users.noreply.github.com>
2020-09-03 17:18:27 -0700

    Update upload.js

--- uploads/upload.js
+++ uploads/upload.js
@@ -1,8 +1,21 @@
-let formData = new FormData();  
-upload = [];
-upload.push(document.querySelector('body > div > div.editor > table > tbody > tr:nth-child(3) > td > button > form > input:nth-child(1)'));
-formData.append("file", upload[0].files[0]);
-fetch('/uploads', {method: "GET", body: formData})
-  .then((response) => {
-    alert(JSON.parse(response))
+// Trying to recreate PHP $_POST using JS
+var $_POST = () => {
+  if (arguments.length == 0) return document.querySelectorAll('*[name]');
+  else if (arguments.length > 1) throw new Error('$_POST() needs only one parameter')
+  else return document.querySelectorAll(`*[name="${arguments[0]}"]`)
+}
+var $_FILES = $_POST('import_editor').files;
+const formData = new FormData();
+formData.append('upload', $_FILES[0]);
+
+fetch('/saveImage', {
+    method: 'POST',
+    body: formData
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log(data)
+  })
+  .catch(error => {
+    console.error(error)
   });
