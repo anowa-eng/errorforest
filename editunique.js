47ddbc830b326a1bc2b8a69a13539eb68a2a0a5d
anowa-eng <60952646+anowa-eng@users.noreply.github.com>
2020-09-11 07:32:49 -0700

    Update editunique.js

--- editunique.js
+++ editunique.js
@@ -1,10 +1,10 @@
 var code = document.querySelector("e").innerHTML.split("\n"),
-  real = code.replace(/(\/\*.*\*\/)|(\/{2}.*)|(#.*)|(\(?[cC][oO]([mM]{2})[eE][nN][tT]\s*([:|-])\s*.*\)?)/g, ""),
-    currentLine;
+real = code.replace(/(\/\*.*\*\/)|(\/{2}.*)|(#.*)|(\(?[cC][oO]([mM]{2})[eE][nN][tT]\s*([:|-])\s*.*\)?)/g, ""),
+currentLine;
 for (var i = 0; i < real.length; i++) {
   currentLine = real[i];
   if (/\bappend\b\belement\b.*/i.test(currentLine)) {
-    if (currentLine.includes) {
+    if (currentLine.includes(' to ')) {
     }
   }
 }
