04efaa9126f83407a2193c9458fa77c615f8e8b1
anowa-eng <60952646+anowa-eng@users.noreply.github.com>
2020-09-11 07:45:16 -0700

    Update editunique.js

--- editunique.js
+++ editunique.js
@@ -1,11 +1,8 @@
 var code = document.querySelector("e").innerHTML.split("\n"),
-real = code.replace(/(\/\*.*\*\/)|(\/{2}.*)|(#.*)|(\(?[cC][oO]([mM]{2})[eE][nN][tT]\s*([:|-])\s*.*\)?)/g, ""),
 currentLine;
-for (var i = 0; i < real.length; i++) {
-  currentLine = real[i];
-  if (/\bappend\b\belement\b.*/i.test(currentLine)) {
-    if (currentLine.includes(' to ')) {
-    }
-  }
-}
-delete window.i;
+document.querySelector('.editor2').innerHTML = document
+  .querySelector('.editor2')
+  .innerHTML
+  .replace('(', '<')
+  .replace(')', '>')
+  .replace(/(\/\*.*\*\/)|(\/{2}.*)|(#.*)|(\(?[cC][oO]([mM]{2})[eE][nN][tT]\s*([:|-])\s*.*\)?)/g, "");
