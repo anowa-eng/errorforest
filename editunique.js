4eff12c46debe93a1a55e6cbc094084dd646266b
anowa-eng <60952646+anowa-eng@users.noreply.github.com>
2020-09-11 07:49:34 -0700

    Delete editunique.js

--- editunique.js
+++
@@ -1,8 +0,0 @@
-var code = document.querySelector("e").innerHTML.split("\n"),
-currentLine;
-document.querySelector('.editor2').innerHTML = document
-  .querySelector('.editor2')
-  .innerHTML
-  .replace('(', '<')
-  .replace(')', '>')
-  .replace(/(\/\*.*\*\/)|(\/{2}.*)|(#.*)|(\(?[cC][oO]([mM]{2})[eE][nN][tT]\s*([:|-])\s*.*\)?)/g, "");
