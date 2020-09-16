9b3cb0f803878b5f51aa5015159f015b28397e06
anowa-eng <60952646+anowa-eng@users.noreply.github.com>
2020-09-16 16:01:51 -0700

    Create errorforest-preprocessor.js

---
+++ errorforest-preprocessor.js
@@ -0,0 +1,53 @@
+const anova01 = {
+  errorforest: {
+    editor2: {
+      preprocessor: {
+        root(element) {
+          if (element.hasAttribute('root')) return element.getAttribute('root');
+          else return undefined;
+        }
+      }
+    }
+  },
+  loops: {
+    count: null,
+    repeat(times, f) {
+      count = 1;
+      for (var i; i < times; i++) {
+        f();
+        count++;
+      }
+      delete window.i;
+      count = null;
+    },
+    skip() {
+      count++;
+    }
+  }
+};
+
+var real;
+const currentLine = real[anova01.loops.count];
+document.querySelectorAll('errorforest-preprocessor').forEach(function(x){
+  real = x.innerHTML
+    .replace(/(\/\*.*\*\/)|(\/{2}.*)|(#.*)|(\(?[cC][oO]([mM]{2})[eE][nN][tT]\s*([:|-])\s*.*\)?)/g, '')
+    .replace(/\bappend\s*.*\b/g, '<')
+    .replace(/\belement(.*)\b$/g, '>')
+    .split('\n')
+    .map(i => `${i};`)
+    .replace(/((;;)$)|(};(;?))/, ';')
+  anova01.loops.repeat(real.length, function(){
+    rt = anova01.errorforest.editor2.preprocessor.root(x) ? document.querySelector(x.getAttribute('root')) : document.body;
+    // Execute lines one by one
+      if (/append .* element (to ((["'].*["'])|(#[0-9]+)?) (as [a-zA-Z0-9_]+)?)/g.test(currentLine)) {
+        if (!(currentLine.split(' ').filter(i => i != new String())[1])) throw new Error('Unexpected semicolon');
+        else {
+          window[`E${rt.children.length}`]  = document.createElement(
+            currentLine.split(' ').filter(i => i != new String())[1]
+          );
+          window[`E${rt.children.length}`].appendChild(rt);
+          window[`E${rt.children.length}`].setAttribute('errorforest-preprocessor-append-status', 'append');
+        }
+      }
+	});
+});
