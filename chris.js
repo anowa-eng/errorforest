f6af07aa004236dcd4c515e447931f41496a76ab
anowa-eng <60952646+anowa-eng@users.noreply.github.com>
2020-09-19 10:25:30 -0700

    Update chris.js

--- chris.js
+++ chris.js
@@ -1,14 +1,3 @@
-// Get query value in website query string – just in case
-// Borrowed from Chris's post "Get URL Variables"
-function Q(variable) {
-       var query = window.location.search.substring(1);
-       var vars = query.split("&");
-       for (var i = 0; i < vars.length; i++) {
-               var pair = vars[i].split("=");
-               if(pair[0] == variable) {
-                       return pair[1];
-               }
-       }
-       delete window.i;
-       return(false);
+function Q(e) {
+  return GQ()[e];
 }
